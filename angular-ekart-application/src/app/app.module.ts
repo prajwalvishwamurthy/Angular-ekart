db52be9a02cfe6158c29062259735e2916587d69
prajwal <prajwal>
2024-04-14 22:36:31 +0530

    made the change in the style.css file and removed the app.component.css file and placed in the style.css file

--- angular-ekart-application/src/app/app.module.ts
+++ angular-ekart-application/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
+import { TopheaderComponent } from './topheader/topheader.component';
+import { TopMenuComponent } from './Header/top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopheaderComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
