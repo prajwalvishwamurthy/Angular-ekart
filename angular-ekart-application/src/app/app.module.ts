8de8a7e620b8a42da12640a41e5f566dc5af4803
prajwal <prajwal>
2024-04-14 23:35:10 +0530

    Complete header component written in multiply new component and finished with the header component

--- angular-ekart-application/src/app/app.module.ts
+++ angular-ekart-application/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
 import { TopheaderComponent } from './topheader/topheader.component';
 import { TopMenuComponent } from './Header/top-menu/top-menu.component';
+import { MainMenuComponent } from './Header/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TopheaderComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
