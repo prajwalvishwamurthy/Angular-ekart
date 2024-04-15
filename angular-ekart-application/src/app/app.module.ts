37fd3dca1ff9fb1fa3ce8de62e68aed1d2d6cd38
prajwal <prajwal>
2024-04-15 23:51:37 +0530

    data binding for 2 way data and 1 way data

--- angular-ekart-application/src/app/app.module.ts
+++ angular-ekart-application/src/app/app.module.ts
@@ -8,7 +8,8 @@ import { TopheaderComponent } from './topheader/topheader.component';
 import { TopMenuComponent } from './Header/top-menu/top-menu.component';
 import { MainMenuComponent } from './Header/main-menu/main-menu.component';
 import { ProductListComponent } from './product-list/product-list.component';
-
+import { SearchComponent } from './product-list/search/search.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +17,13 @@ import { ProductListComponent } from './product-list/product-list.component';
     TopheaderComponent,
     TopMenuComponent,
     MainMenuComponent,
-    ProductListComponent
+    ProductListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
